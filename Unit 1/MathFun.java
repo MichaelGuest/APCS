719f5c4151781476d811acb5f695c8b820d9f250
Michael <Michaelguest15@gmail.com>
2015-09-10 23:34:57 -0700

    Unit 1

---
+++ Unit 1/MathFun.java
@@ -0,0 +1,29 @@
+package MathFun;
+
+public class MathFun {
+
+	public static void main (String[]args_) {
+		byte problem_1 = 4+9;
+		short problem_2 = 46 / 7;
+		long problem_3 =46 % 7;
+		double problem_4 = 2 * 3.0;
+		double problem_5 = (double)25 / 4;
+		int problem_6 =(int) 7.75 + 2;
+		int problem_7 =(int) 'P';
+		char problem_8 =(char) 105; 
+		
+		System.out.println(problem_1);
+		System.out.println(problem_2);
+		System.out.println(problem_3);
+		System.out.println(problem_4);
+		System.out.println(problem_5);
+		System.out.println(problem_6);
+		System.out.println(problem_7);
+		System.out.println(problem_8);
+	
+	
+	
+		
+		
+	}
+}
