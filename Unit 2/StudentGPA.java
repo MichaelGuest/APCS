266e9de015739be37dd6e45dead19903fe6e8f7a
Michael <Michaelguest16@gmail.com>
2015-09-15 13:30:21 -0700

    Update StudentGPA.java

--- Unit 2/StudentGPA.java
+++ Unit 2/StudentGPA.java
@@ -5,70 +5,79 @@ import chn.util.ConsoleIO;
 public class StudentGPA
 {
 
-    private String myName;
-    private String myID;
-    int myTotalPoints;
-    int myNumGrades;
-    private double myGPA;
+	private String myName;
+	private String myID;
+	private int myTotalPoints;
+	private int myNumGrades;
+	private double myGPA;
 
-//constructor
-public StudentGPA (String name, String id)
-{
-    myName = name;
-    myID = id;
-    myTotalPoints = 0;
-    myNumGrades = 0;
+	/**
+	 * 
+	 * @param name
+	 * @param id
+	 */
+	public StudentGPA (String name, String id)
+	{
+		myName = name;
+		myID = id;
+		myTotalPoints = 0;
+		myNumGrades = 0;
+	}
+	/**
+	 * Adds Grades
+	 * @param grade
+	 */
+	public void addGrade(int grade)
+	{
+		myTotalPoints += grade;
+		myNumGrades++;
+	}
+
+	public double getGPA()
+	{
+		if(myNumGrades == 0)
+		{
+			return 0.0;
+		}
+		return myGPA = (double)
+				myTotalPoints / myNumGrades;
+	}
+	
+
+	
+	//Main
+	public static void main(String[] args)
+	{
+
+		int grade;
+
+		ConsoleIO console = new ConsoleIO();
+
+		System.out.print("Enter Student Name: ");
+		String name = console.readLine();
+
+		System.out.print("Enter Student ID: ");
+		String id = console.readLine();
+
+		StudentGPA gpa = new StudentGPA(name, id);
+		System.out.println("initial GPA: " + gpa.getGPA());
+
+		System.out.print("Enter a grade: ");
+		grade = console.readInt();
+		gpa.addGrade(grade);
+		System.out.println("GPA: " + gpa.getGPA());
+
+		System.out.print("Enter a grade: ");
+		grade = console.readInt();
+		gpa.addGrade(grade);
+		System.out.println("GPA: " + gpa.getGPA());
+
+		System.out.print("Enter a grade: ");
+		grade = console.readInt();
+		gpa.addGrade(grade);
+		System.out.println("GPA: " + gpa.getGPA());
+
+	}
+
 }
-    //methods
-   public void addGrade(int grade)
-    {
-        myTotalPoints += grade;
-        myNumGrades++;
-    }
-
-public double getGPA()
-{
-    if(myNumGrades == 0)
-    {
-        return 0.0;
-    }
-    return myGPA = (double)
-    myTotalPoints / myNumGrades;
-}
-    public static void main(String[] args)
-    {
-
-        int grade;
-
-        ConsoleIO console = new ConsoleIO();
-
-        System.out.print("Enter Student Name: ");
-        String name = console.readLine();
-
-        System.out.print("Enter Student ID: ");
-        String id = console.readLine();
-
-        StudentGPA gpa = new StudentGPA(name, id);
-        System.out.println("initial GPA: " + gpa.getGPA());
-
-        System.out.print("Enter a grade: ");
-        grade = console.readInt();
-        gpa.addGrade(grade);
-        System.out.println("GPA: " + gpa.getGPA());
-
-        System.out.print("Enter a grade: ");
-        grade = console.readInt();
-        gpa.addGrade(grade);
-        System.out.println("GPA: " + gpa.getGPA());
-
-        System.out.print("Enter a grade: ");
-        grade = console.readInt();
-        gpa.addGrade(grade);
-        System.out.println("GPA: " + gpa.getGPA());
-
-    }
-
-
-}
-
 
