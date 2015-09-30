ffa91a7c4ddf2ff3ba5633e85c0e82c1537ac5e7
Michael <Michaelguest16@gmail.com>
2015-09-29 22:02:13 -0700

    Taxpayer Tester.java

---
+++ Unit 2/IRS/Taxpayer Tester.java
@@ -0,0 +1,29 @@
+package IRS;
+
+import chn.util.ConsoleIO;
+
+public class App {
+
+    public static void main(String[] args) {
+        ConsoleIO console = new ConsoleIO();
+
+        String statusStr;
+        System.out.println("Filing status: Single or Married");
+        statusStr = console.readToken();
+        char status = statusStr.charAt(0);
+
+        System.out.println("Enter Taxable Income:");
+        double income = console.readDouble();
+
+
+        Taxpayer person = new Taxpayer(status, income);
+        
+        System.out.println(person.getStatus());
+        System.out.println("Taxable income = $" + person.getIncome());
+        System.out.println("Federal tax = $" + person.calculateTax());
+
+    }
+}
+
+
+
