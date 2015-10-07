12fd485934f95db4cbaf3af8856b0793006cfb96
Michael <Michaelguest16@gmail.com>
2015-10-06 22:37:48 -0700

    Not sure why 12.00 output doesn't print. Everything else is good.

--- Unit-3/LoanTable.java
+++ Unit-3/LoanTable.java
@@ -7,48 +7,39 @@ public class loanTable {
 
 
     public static void main(String[] args) {
-        //Declaring Variables
-        double k;
-        double n;
-        double c;
-        double p;
-        double a;
-        double low;
-        double high;
-
-        int time;
 
         //Getting User Input
         ConsoleIO console = new ConsoleIO();
 
         System.out.println("Amount of the loan: ");
-        p = console.readDouble();
+        double p = console.readDouble();
         System.out.println("Principal = $" + p);
 
         System.out.println("The length of loan in years: ");
-        time = console.readInt();
+        int time = console.readInt();
         System.out.println("Time = " + time + " years");
 
         System.out.println("A low interest rate in %: ");
-        low = console.readDouble() / 100;
-        System.out.println("Low rate = " + low + "%");
+        double lowInt = console.readDouble() / 100;
+        System.out.println("Low rate = " + lowInt + "%");
 
         System.out.println("A high interest rate in %: ");
-        high = console.readDouble() / 100;
-        System.out.println("High rate = " + high + "%");
+        double highInt = console.readDouble() / 100;
+        System.out.println("High rate = " + highInt + "%");
 
         //While Loop
-        while (low <= high) {
+        while (lowInt <= highInt) {
 
-            k = low / 1200.0;
-            n = time * 12;
-            c = Math.pow((1 + k), n);
-            a = (p * k * c) / (c - 1);
+            double k = lowInt / 12;
+            double n = time * 12;
+            double c = Math.pow((1 + k), n);
+            double a = (p * k * c) / (c - 1);
 
-            System.out.println("Annual interest rate: " + low * 100);
+            System.out.println("Annual interest rate: " + lowInt * 100);
             System.out.println("Monthly Payment: " + Format.left(a, 2, 2));
 
-            low += (0.25 / 100);
+            //Incrementing by .25
+            lowInt += (0.25 / 100);
         }
     }
 
