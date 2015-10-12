9942eebb30733b47e2a028bcb7a4e656cd9ca3c3
Michael <Michaelguest16@gmail.com>
2015-10-11 18:08:13 -0700

    PicturesApp.java

---
+++ Unit-3/Pictures/PicturesApp.java
@@ -0,0 +1,24 @@
+package pictures;
+
+import chn.util.*;
+
+public class PicturesApp {
+    public static void main(String[] args) {
+
+        ConsoleIO keyboard = new ConsoleIO();
+
+        Pictures picture = new Pictures();
+        String get;
+
+        Pictures.printTable(4, 6);
+        get = keyboard.readLine(); // freezes the output screen to see the picture
+        Pictures.printTable(11, 12);
+        get = keyboard.readLine();
+        Pictures.pyramid(10);
+        get = keyboard.readLine();
+        Pictures.pyramid(25);
+        get = keyboard.readLine();
+
+    }
+
+}
